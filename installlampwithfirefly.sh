7f05fa0103fb4d5c5745a426a091ee73ddce8827
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-12-20 09:29:40 -0500

    Update installlampwithfirefly.sh

--- installlampwithfirefly.sh
+++ installlampwithfirefly.sh
@@ -10,7 +10,7 @@ if grep -q Debian "/etc/os-release" ; then
 	sudo apt install -y curl wget gnupg2 ca-certificates lsb-release apt-transport-https
 	wget https://packages.sury.org/php/apt.gpg
 	sudo apt-key add apt.gpg
-  echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.list
+  echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php8.list
 else
 	echo "Not Debian...continuing"
 fi
@@ -19,7 +19,7 @@ fi
 sudo apt update && sudo apt upgrade -y
 
 # Install web components
-sudo apt install apache2 mysql-common mariadb-server php7.4 php7.4-bcmath php7.4-intl php7.4-curl php7.4-zip php7.4-gd php7.4-xml php7.4-mbstring php7.4-ldap php7.4-mysql php-mysql -y
+sudo apt install apache2 mysql-common mariadb-server php8.0 php8.0-bcmath php8.0-intl php8.0-curl php8.0-zip php8.0-gd php8.0-xml php8.0-mbstring php8.0-ldap php8.0-mysql php-mysql -y
 echo
 echo "Installing Composer (a friendly php helper that unpacks the php libraries contained within firefly and creates a firefly-iii project)..."
 echo
@@ -29,7 +29,7 @@ cd /var/www/html
 echo
 echo "If prompted, just hit Enter"
 echo
-sudo composer create-project grumpydictator/firefly-iii --no-dev --prefer-dist firefly-iii 5.4.6
+sudo composer create-project grumpydictator/firefly-iii --no-dev --prefer-dist firefly-iii 5.6.5
 
 # This will stop the  white screen issue
 # Changing firefly-iii folder permissions
@@ -39,7 +39,7 @@ echo
 echo "Creating firefly database environment..."
 echo
 echo "Enter your MySQL root password.  If you don't have one, just hit Enter."
-sudo mysql -u root -p < $HOME/firefly-iii-automation/mysql_setup.txt
+sudo mysql -u root -p < $HOME/firefly-iii-automation/mysql_setup.sql
 sudo cp $HOME/firefly-iii-automation/.env /var/www/html/firefly-iii/
 
 # Editing apache to allow modules
