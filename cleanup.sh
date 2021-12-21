30f071aae4c40c35db81d1c1372d4196dee68094
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-12-20 22:16:28 -0500

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -4,11 +4,11 @@
 echo "This will reset the stage so you can start the installation from scratch"
 cd
 sudo rm -rf /var/www/html/firefly-iii
-sudo rm -rf firefly-iii-automation
 echo
 echo "[ DONE ]"
 echo
 echo "Removing web components"
 sudo apt remove --purge apache2 mysql* mariadb* php* -y
+sudo rm -rf firefly-iii-automation
 echo
 echo "[ DONE ]"
