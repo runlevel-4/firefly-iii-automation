06388fa9ada308cacd1f7cde52b6f47fd0ebd3c8
runlevel4 <90661907+runlevel-4@users.noreply.github.com>
2021-12-30 13:36:41 -0500

    Update installlampwithfirefly.sh

--- installlampwithfirefly.sh
+++ installlampwithfirefly.sh
@@ -13,12 +13,14 @@ if grep -q Debian "/etc/os-release" ; then
   echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php8.list
 else
 	echo "Not Debian...continuing"
+	echo
+	echo "Adding Ubuntu PHP repos"
+	echo
+	# Add the PHP 8.0 repo
+	sudo apt install ca-certificates apt-transport-https software-properties-common -y
+	sudo add-apt-repository ppa:ondrej/php
+	sudo add-apt-repository ppa:ondrej/apache2
 fi
-
-# Add the PHP 8.0 repo
-sudo apt install ca-certificates apt-transport-https software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php
-sudo add-apt-repository ppa:ondrej/apache2
 
 # Perform updates
 sudo apt update && sudo apt upgrade -y
